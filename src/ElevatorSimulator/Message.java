82b5a1bd89e6af7669961352a08d1fbd2d919c13
guymorgenshtern <33492457+guymorgenshtern@users.noreply.github.com>
2023-02-02 13:44:59 -0500

    Floor content done
    read from file, generate requests from CSV, send and receive from scheduler

--- src/ElevatorSimulator/Message.java
+++ src/ElevatorSimulator/Message.java
@@ -12,29 +12,15 @@ package ElevatorSimulator;
 public class Message {
 	SenderType sender;
 	String timestamp;
-	int floorNumber;
-	String content;
-	MessageType type;
 	
-	Message(SenderType sender, String timestamp, int floorNumber, String content, MessageType type){
+	Message(SenderType sender, String timestamp){
 		this.sender = sender;
 		this.timestamp = timestamp;
-		this.floorNumber = floorNumber;
-		this.content = content;
-		this.type = type;
 	}
 	
 	String getDescription() {
-		return timestamp + "\n" + floorNumber + "\n" + content + "\n" + type;
+		return timestamp + "\n" + sender + "\n";
 	}
-}
-
-enum MessageType{
-	READY_TO_MOVE,
-	REQUEST_ELEVATOR,
-	MOVE_TO,
-	ADD_STOP,
-	ARRIVED,
 }
 
 enum SenderType {
--- src/ElevatorSimulator/Simulator.java
+++ src/ElevatorSimulator/Simulator.java
@@ -20,11 +20,14 @@ public class Simulator {
 		
 		schedulerThread = new Thread(scheduler, "Scheduler");
 		elevatorThread = new Thread(new Elevator(scheduler), "ELEVATOR");
-		floorThread = new Thread(new Floor(scheduler), "FLOOR");
+		floorThread = new Thread( new Floor(scheduler, "src/ElevatorSimulator/resources/elevator_input.csv"), "FLOOR");
+		
+		
 		
 		schedulerThread.start();
 		elevatorThread.start();
 		floorThread.start();
+		
 	}
 
 }
